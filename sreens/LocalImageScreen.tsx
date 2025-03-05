932d86898628ad461d18c32b1563485237082708
nik-markiza <rufus_anon@yahoo.com>
2025-03-05 00:59:49 -0600

    LocalImage cleanup

--- sreens/LocalImageScreen.tsx
+++ sreens/LocalImageScreen.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useCallback, useMemo } from 'react';
 import {
   View, Text, StyleSheet, Image, Pressable, Platform, ScrollView,
   Alert, Share,
@@ -17,12 +17,12 @@ const LocalImageScreen: FC = () => {
   const router = useRouter();
   const { imageURI, metadata, chooseImage } = useImageMetadata();
 
-  const openMap = () => {
+  const openMap = useCallback(() => {
     router.push({
       pathname: '/map',
       params: metadata?.gpsLocation ?? {},
     });
-  };
+  }, [metadata?.gpsLocation, router]);
 
   const handleShare = async (uri: string) => {
     try {
@@ -74,43 +74,59 @@ const LocalImageScreen: FC = () => {
     }
   };
 
-  const DISABLED_BUTTON = !metadata;
+  const SensitiveBlock = useMemo((): React.ReactNode => {
+    if (!metadata) return <View />;
 
-  return (
-    <SafeAreaView edges={['bottom']} style={styles.container}>
-      <Pressable onPress={chooseImage} style={styles.imageContainer}>
-        {
+    const {
+      fileName, originalDate, resolution, fileSize, extension, gpsLocation, device, model, software,
+    } = metadata;
+
+    return (
+      <View style={styles.sensitiveContainer}>
+        <Text style={styles.sensitiveTitle}>Sensitive Data</Text>
+        <View style={styles.separator} />
+        <Text style={styles.infoText}>{`Name: ${fileName}`}</Text>
+        {metadata.originalDate && <Text style={styles.infoText}>{`Original date: ${originalDate}`}</Text>}
+        <Text style={styles.infoText}>{`Extension: ${extension}`}</Text>
+        <Text style={styles.infoText}>{`Size: ${fileSize}`}</Text>
+        {resolution && <Text style={styles.infoText}>{`Resolution: ${resolution}`}</Text>}
+        {!!gpsLocation && (
+          <Pressable
+            onPress={openMap}
+            style={({ pressed }) => [styles.mapButton, pressed && styles.op7]}
+          >
+            <Text style={styles.infoText}>{'Location: found. '}</Text>
+            <Text style={[styles.infoText, { color: '#3742fa' }]}>Show on map?</Text>
+            <Feather style={styles.featherIcon} name="map-pin" size={18} color="black" />
+          </Pressable>
+        )}
+        {metadata.device && <Text style={styles.infoText}>{`Device: ${device} ${model} (${software})`}</Text>}
+      </View>
+    );
+  }, [metadata, openMap]);
+
+  const ImageBlock = useMemo(() : React.ReactNode => (
+    <Pressable onPress={chooseImage} style={styles.imageContainer}>
+      {
         imageURI
           ? <Image resizeMode="contain" style={[styles.image, styles.shadow]} source={{ uri: imageURI }} />
           : <Text style={{ fontSize: 24 }}>Press to choose image</Text>
         }
-      </Pressable>
+    </Pressable>
+  ), [chooseImage, imageURI]);
+
+  const DISABLED_BUTTON = !metadata;
+
+  return (
+    <SafeAreaView edges={['bottom']} style={styles.container}>
+      {ImageBlock}
       <View style={styles.infoContainer}>
         {metadata && (
         <ScrollView
-          contentContainerStyle={styles.contentContainer}
           style={styles.scrollContaintainre}
+          contentContainerStyle={styles.contentContainer}
         >
-          <View style={{ borderWidth: 2, borderColor: 'black', paddingVertical: 5, borderRadius: 5, marginHorizontal: 5 }}>
-            <Text style={{ alignSelf: 'center', fontSize: 20}}>Sensetive Data</Text>
-            <View style={{ backgroundColor: 'black', width: '100%', height: 2, marginTop: 5 }} />
-            <Text style={styles.infoText}>{`Name: ${metadata.fileName}`}</Text>
-            {metadata.originalDate && <Text style={styles.infoText}>{`Original date: ${metadata.originalDate}`}</Text>}
-            <Text style={styles.infoText}>{`Extension: ${metadata.extension}`}</Text>
-            <Text style={styles.infoText}>{`Size: ${metadata.fileSize}`}</Text>
-            {metadata.resolution && <Text style={styles.infoText}>{`Resolution: ${metadata.resolution}`}</Text>}
-            {metadata.gpsLocation && (
-              <Pressable
-                onPress={openMap}
-                style={({ pressed }) => [styles.mapButton, pressed && styles.op7]}
-              >
-                <Text style={styles.infoText}>{'Location: found. '}</Text>
-                <Text style={[styles.infoText, { color: '#3742fa' }]}>Show on map?</Text>
-                <Feather style={styles.featherIcon} name="map-pin" size={18} color="black" />
-              </Pressable>
-            )}
-            {metadata.device && <Text style={styles.infoText}>{`Device: ${metadata.device} ${metadata.model} (${metadata.software})`}</Text>}
-          </View>
+          {SensitiveBlock}
         </ScrollView>
         )}
       </View>
@@ -216,6 +232,23 @@ const styles = StyleSheet.create({
   pathContainer: {
     flexShrink: 1,
     justifyContent: 'center',
+  },
+  sensitiveContainer: {
+    borderWidth: 2,
+    borderColor: 'black',
+    paddingVertical: 5,
+    borderRadius: 5,
+    marginHorizontal: 5,
+  },
+  sensitiveTitle: {
+    alignSelf: 'center',
+    fontSize: 20,
+  },
+  separator: {
+    backgroundColor: 'black',
+    width: '100%',
+    height: 2,
+    marginTop: 5,
   },
 });
 
